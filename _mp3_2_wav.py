08af61713b6b401b76d836b01cbe2acb1fba0cab
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-12-25 12:55:44 +0530

    Update _mp3_2_wav.py

--- _mp3_2_wav.py
+++ _mp3_2_wav.py
@@ -4,7 +4,7 @@ import os
 
 
 #read the folder for files
-folder_path = str(input("Enter the cv-corpus-audios folder path: "))
+folder_path = str(input("Enter the cv-corpus-clips folder path: "))
 lst_mp3 = os.listdir(folder_path)
 count_mp3 = 0
 for i in lst_mp3:
