e67842db6a73f74a7a578611130652b7bf62553f
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-12-25 12:54:15 +0530

    Create _mp3_2_wav.py

---
+++ _mp3_2_wav.py
@@ -0,0 +1,15 @@
+import pydub
+import multiprocessing as mp
+import os
+
+
+#read the folder for files
+folder_path = str(input("Enter the cv-corpus-audios folder path: "))
+lst_mp3 = os.listdir(folder_path)
+count_mp3 = 0
+for i in lst_mp3:
+    if i.endswith('.mp3'):
+        count_mp3 += 1
+    else:
+        continue
+print('No of mp3 files found: ', count_mp3)
