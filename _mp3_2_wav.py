64a4b7dec610a34f4e1fdf61984620cacaa2ec22
Devasheesh Mishra <swastik.mishra.c322.smart@gmail.com>
2022-12-25 23:02:18 +0530

    .

--- _mp3_2_wav.py
+++ _mp3_2_wav.py
@@ -3,70 +3,81 @@ from pydub import AudioSegment
 import threading as th
 import os
 from time import sleep
+from time import time
 # import argparse
 
 # max_processes = 100
 max_threads = 400
-
-#read the folder for files
+current_time = time()
+# read the folder for files
 # folder_path = str(input("Enter the cv-corpus-clips folder path: "))
 # wave_path = str(input("Enter the output folder path: "))
 folder_path = r"clips_test"
 wave_out_path = r"wav_audio_dataset"
-lst_mp3 = os.listdir(folder_path)
+lst_files = os.listdir(folder_path)
 count_mp3 = 0
 index = 1
 
-for i in lst_mp3:
+
+for i in lst_files:
     if i.endswith('.mp3'):
         count_mp3 += 1
 
     else:
         continue
-print(str(count_mp3) +" "+"files found")
+print('Total number of files in the folder: ', len(lst_files))
+print(".mp3 files found: ", count_mp3)
 
-def temp():
-    r=9542358932693*9295239632
-    return r
-        
+
+# def temp():
+#     r = 9542358932693*9295239632
+#     return r
+
+
 def convert(mp3_path, wave_path):
     # print(mp3_path)
     # print(wave_path)
     mp3 = AudioSegment.from_mp3(mp3_path)
     mp3.export(wave_path, format="wav")
-        
+
+
 if __name__ == '__main__':
     # print('No of .mp3 files found: ', count_mp3)
-    for mp3 in lst_mp3:
+    for mp3 in lst_files:
         if mp3.endswith('.mp3'):
             mp3_path = folder_path + '\\' + mp3
-            wave_path = wave_out_path + '\\' + mp3.replace('.mp3','.wav')
-            print("converting file " + str(index) + "/" + str(count_mp3) + " to wav", end="\r")
-            index = index + 1
+            wave_path = wave_out_path + '\\' + mp3.replace('.mp3', '.wav')
+            print("converting file " + str(index) + "/" + str(count_mp3) + " to wav" +
+                  '\t \t' + 'Time passed: ', round(time() - current_time, 2), 'seconds', end="\r")
+            # print('Time passed: ', time() - current_time, 'seconds', end='\r')
+            index += 1
 
             while True:
                 # mp.Process(target=temp).start()
-                th.Thread(target=temp).start()
-                
+                # th.Thread(target=temp).start()
+
                 # print('{} processes are running',len(mp.active_children()))
-                
+
                 # if len(mp.active_children())<max_processes:
-                if len(th.enumerate())<max_threads:
+                # print(len(th.enumerate()))
+                if len(th.enumerate()) < max_threads:
                     # print(mp.active_children())
                     # p = mp.Process(target=convert, args=(mp3_path, wave_path))
-                    p= th.Thread(target=convert, args=(mp3_path, wave_path))
+                    p = th.Thread(target=convert, args=(mp3_path, wave_path))
                     p.start()
                     break
                 else:
-                    # print('-'*50)
+                    print('-'*50)
                     print('Waiting for a thread to finish...')
                     # print('Waiting for a process to finish...')
                     # print('Active processes: ', mp.active_children())
+                    print('Active threads: ', len(th.enumerate()))
                     # print(mp3)
-                    # print('-'*50)
+                    print('-'*50)
                     sleep(.5)
-                
+
         else:
-            print("{} not a mp3 file", mp3)
-            
-print("Congratulations your files have been  successfully converted to wav file from mp3 !!!")
+            print("'{}' not a mp3 file".format(mp3))
+
+    print("Congratulations '{}' files have been successfully converted to wave in '{}' seconds".format(index-1, round(time() - current_time, 2)))
+    print('Conversion Speed: "{}" files per second: '.format(round((index-1)/(time() - current_time), 2)))
