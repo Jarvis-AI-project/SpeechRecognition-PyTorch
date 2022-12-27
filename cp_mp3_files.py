4de8eb7462207f04cc1df49a758454bf0bd0f495
Devasheesh Mishra <72092910+devasheeshG@users.noreply.github.com>
2022-12-27 16:24:00 +0530

    Update cp_mp3_files.py

--- cp_mp3_files.py
+++ cp_mp3_files.py
@@ -1,14 +1,14 @@
 import threading as th
 import os
 import time
-max_threads = 250
+max_threads = 50
 
-#read master folder
+# read master folder
 master_folder = r'D:\cv-corpus-12.0-2022-12-07\en\clips'
 destination_folder = r"C:\cv-corpus-12.0-2022-12-07\en\clips"
 lst_files = os.listdir(master_folder)
 # global skip, index
-skip, index = 0,0
+skip, index = 0, 0
 
 print("Total number of files in the folder: ", len(lst_files))
 
@@ -16,27 +16,30 @@ print("Total number of files in the folder: ", len(lst_files))
 #     command = "xcopy {} {}".format(source, destination)
 #     return command
 
+
 def copy_file(file_name):
     if os.path.exists(destination_folder+'\\'+file_name):
         global skip
-        skip+=1
+        skip += 1
         return
-    command = command = "xcopy {} {}".format(master_folder+'\\'+file_name, destination_folder)
+    command = command = "xcopy {} {}".format(
+        master_folder+'\\'+file_name, destination_folder)
     while True:
         if len(th.enumerate()) < max_threads:
             th.Thread(target=os.system, args=(command,)).start()
             global index
-            index+=1
+            index += 1
             return
         else:
             time.sleep(0.1)
-    
-    
+
+
 def status():
     time.sleep(3)
     while True:
-        print("Files copied: '{}' | Files Skipped: '{}'".format(index, skip), end='\r')
-        
+        print("Files copied: '{}' | Files Skipped: '{}'".format(
+            index, skip), end='\r')
+
 
 th.Thread(target=status).start()
 
